93aa8efd5ead4b8741a8d47fbf5587fd12d07e7b
Bobby <bobbychopra@gmail.com>
2015-03-07 15:02:00 -0500

    Changes to make lesson 6 work

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -60,14 +60,19 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly ActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         public TailActor(ActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
+        }
+
+        protected override void PreStart()
+        {
+            base.PreStart();
 
             // start watching file for changes
             _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
@@ -82,6 +87,21 @@ namespace WinTail
             // read the initial contents of the file and send it to console as first message
             var text = _fileStreamReader.ReadToEnd();
             Self.Tell(new InitialRead(_filePath, text));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void PostStop()
+        {
+            //NOTE: What if we threw another exception in PostStop??
+            _observer.Dispose();
+            _observer = null;
+
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            _fileStream.Dispose();
+            base.PostStop();
         }
 
         protected override void OnReceive(object message)
