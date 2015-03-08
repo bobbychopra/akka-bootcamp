ac5f45ad5cbfc6b6898b7a678fbb9fb6e2ada300
Bobby <bobbychopra@gmail.com>
2015-03-08 10:01:02 -0400

    Changes to make lesson 1 work

--- src/Unit-2/DoThis/Program.cs
+++ src/Unit-2/DoThis/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
 using Akka.Actor;
+using Akka.Configuration.Hocon;
+using System.Configuration;
 
 namespace ChartApp
 {
@@ -18,7 +20,10 @@ namespace ChartApp
         [STAThread]
         static void Main()
         {
-            ChartActors = ActorSystem.Create("ChartActors");
+            var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
+            var config  = section.AkkaConfig;
+
+            ChartActors = ActorSystem.Create("ChartActors", config);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
