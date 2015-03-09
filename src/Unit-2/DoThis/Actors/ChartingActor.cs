bcb02b4fd3f6aeee7b2e83b0af7b2e2a510ec76d
Bobby <bobbychopra@gmail.com>
2015-03-08 23:27:08 -0400

    Changes to make lesson 2 work

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -6,9 +6,22 @@ using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : UntypedActor
+    public class ChartingActor : ReceiveActor
     {
         #region Messages
+
+        /// <summary>
+        /// Add a new <see cref="Series"/> to the chart
+        /// </summary>
+        public class AddSeries
+        {
+            public Series Series { get; set; }
+
+            public AddSeries(Series series)
+            {
+                Series = series;
+            }
+        }
 
         public class InitializeChart
         {
@@ -33,15 +46,10 @@ namespace ChartApp.Actors
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
-        }
 
-        protected override void OnReceive(object message)
-        {
-            if (message is InitializeChart)
-            {
-                var ic = message as InitializeChart;
-                HandleInitialize(ic);
-            }
+
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
         }
 
         #region Individual Message Type Handlers
@@ -66,6 +74,15 @@ namespace ChartApp.Actors
                     series.Value.Name = series.Key;
                     _chart.Series.Add(series.Value);
                 }
+            }
+        }
+
+        private void HandleAddSeries(AddSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
+            {
+                _seriesIndex.Add(series.Series.Name, series.Series);
+                _chart.Series.Add(series.Series);
             }
         }
 
